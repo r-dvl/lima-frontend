a895959a5eb04932e9042cf0ed01f6965869175a
R-dVL <rauldel.valle.lima@gmail.com>
2023-08-18 18:43:26 +0200

    Cat-Watcher Photos view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(() => {
+        fetch('/photos')
+            .then(response => response.json())
+            .then(photosData => {
+                setPhotos(photosData);
+            })
+            .catch(error => {
+                console.error('Error fetching photos:', error);
+            });
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Cat-Watcher</h1>
+            <div className="photo-list">
+                {photos.map(photo => (
+                    <div key={photo.id} className="photo-item">
+                        <img src={`/photos/${photo.id}`} alt={`${photo.id}`} />
+                        <p>Date: {new Date(photo.date).toLocaleString()}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
