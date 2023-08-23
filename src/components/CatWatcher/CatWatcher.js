3b0a75a38916a0e13dfceb7cf1cc03e7c0738021
R-dVL <rauldel.valle.lima@gmail.com>
2023-08-23 21:42:15 +0200

    Add pagination + fix footer

--- src/components/CatWatcher/CatWatcher.js
+++ src/components/CatWatcher/CatWatcher.js
@@ -1,13 +1,18 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './CatWatcher.css';
-import { Paper } from '@mui/material';
+import { Paper, Button } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 function CatWatcher() {
     const [photos, setPhotos] = useState([]);
     const [selectedDate, setSelectedDate] = useState(new Date());
+    const [currentPage, setCurrentPage] = useState(1);
+    const photosPerPage = 9;
 
+    // Get Photos from server
     useEffect(() => {
         fetch('/photos')
             .then(response => response.json())
@@ -19,6 +24,7 @@ function CatWatcher() {
             });
     }, []);
 
+    // Filter Photos by date and time
     const filteredAndSortedPhotos = photos
         .filter(photo => {
             if (!selectedDate) return true;
@@ -31,23 +37,56 @@ function CatWatcher() {
         })
         .sort((a, b) => new Date(b.date) - new Date(a.date));
 
+    // Pagination
+    const indexOfLastPhoto = currentPage * photosPerPage;
+    const indexOfFirstPhoto = indexOfLastPhoto - photosPerPage;
+    const photosToShow = filteredAndSortedPhotos.slice(indexOfFirstPhoto, indexOfLastPhoto);
+    const totalPages = Math.ceil(filteredAndSortedPhotos.length / photosPerPage);
+
     return (
         <div className="container">
+            {/* Calendar */}
             <div className="filters">
                 <DatePicker
                     selected={selectedDate}
                     onChange={date => setSelectedDate(date)}
                     dateFormat="yyyy-MM-dd"
-                    placeholderText="Select a date"
+                    placeholderText="Selecciona una fecha"
                 />
             </div>
+
+            {/* Photos */}
             <div className="photo-list">
-                {filteredAndSortedPhotos.map(photo => (
+                {photosToShow.map(photo => (
                     <Paper key={photo.id} elevation={3} className="photo-item">
                         <img src={`data:image/jpeg;base64,${photo.image}`} alt={`${photo.date}`} />
                         <p>{new Date(photo.date).toLocaleString()}</p>
                     </Paper>
                 ))}
+            </div>
+
+            {/* Pagination */}
+            <div className="pagination">
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => setCurrentPage(currentPage - 1)}
+                    disabled={currentPage === 1}
+                    style={{ marginRight: '10px' }}
+                >
+                    <ArrowBackIcon />
+                </Button>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => setCurrentPage(currentPage + 1)}
+                    disabled={indexOfLastPhoto >= filteredAndSortedPhotos.length}
+                >
+                    <ArrowForwardIcon />
+                </Button>
+                <p style={{ marginLeft: '10px' }}>
+                    Page {currentPage} of {totalPages}
+                </p>
             </div>
         </div>
     );
